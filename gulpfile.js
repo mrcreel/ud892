cccb7ace320d9d5199bab2e1340845b1f682d6a3
mrcreel <michaelrcreel@gmail.com>
2017-12-25 20:32:24 -0600

    Initiak update

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function() {
+  // place code for your default task here
+  console.log("Hello world!");
+});
+
+gulp.task('styles', function () {
+	/* body... */
+  gulp.src('sass/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css'));
+});
