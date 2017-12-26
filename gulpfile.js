caf13256d5f01e43da01fdc99af30a60443245f4
mrcreel <michaelrcreel@gmail.com>
2017-12-25 20:58:58 -0600

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   // place code for your default task here
   console.log("Hello world!");
 });
 
 gulp.task('styles', function () {
-	/* body... */
+  /* body... */
   gulp.src('sass/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./css'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['> 5%'] // 'last 2 versions' 
+    }))
+    .pipe(gulp.dest('./css'));
 });
